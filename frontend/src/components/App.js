ceaf4cb202635107695688cd85cbf2d837fe9f4d
Readri205 <read205@outlook.com>
2021-02-20 15:50:05 +0000

    add react routes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-// import HomePage from "./HomePage";
+import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component {
   constructor(props) {
@@ -8,7 +10,11 @@ export default class App extends Component {
   }
 
   render() {
-    return <h1>Testing React Code</h1>;
+    return (
+    <div>
+    <HomePage />
+    </div>
+    );
   }
 }
 
